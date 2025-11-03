0a5bf23f7d0a7a174f0065453aa9865db7676e4b
Mroik <mroik@delayed.space>
2025-11-03 02:33:30 +0100

    Implement table creation

--- src/data_handler.rs
+++ src/data_handler.rs
@@ -4,18 +4,18 @@ use anyhow::Result;
 use chrono::{Local, TimeDelta};
 use gcal_rs::{Event, EventClient, GCalClient, OAuth};
 use rusqlite::Connection;
+use tokio::sync::Mutex;
 
 /// Handles data retrieval with google's API and persistent data
 struct DataHandler {
     g_client: EventClient,
-    sql_conn: Connection,
+    sql_conn: Mutex<Connection>,
     time_window: Duration,
     chat_id: String,
     calendar_id: String,
 }
 
 impl DataHandler {
-    // TODO: Create DB if not exist
     async fn new(
         time_window: Duration,
         chat_id: &str,
@@ -24,25 +24,27 @@ impl DataHandler {
         client_secret: &str,
         db_loc: &str,
     ) -> Result<Self> {
-        let token = OAuth::new(client_id, client_secret, "http://localhost/")
+        let sql_conn = Connection::open(db_loc)?;
+        create_db(&sql_conn).await?;
+
+        let token = OAuth::new(client_id, client_secret, "http://localhost:5000/auth")
             .naive()
             .await?;
         let (_, g_client) = GCalClient::new(token, None)?.clients();
-        let sql_conn = Connection::open(db_loc)?;
         let chat_id = String::from(chat_id);
         let calendar_id = String::from(calendar_id);
 
         Ok(DataHandler {
             g_client,
-            sql_conn,
+            sql_conn: Mutex::new(sql_conn),
             time_window,
             chat_id,
             calendar_id,
         })
     }
 
-    async fn mark_as_done(&mut self, event_id: &str) -> Result<()> {
-        self.sql_conn.execute(
+    async fn mark_as_done(&self, event_id: &str) -> Result<()> {
+        self.sql_conn.lock().await.execute(
             "INSERT OR IGNORE INTO done (event_id, calendar_id, timestamp) VALUES (?, ?, ?)",
             [
                 event_id,
@@ -53,7 +55,7 @@ impl DataHandler {
         Ok(())
     }
 
-    async fn get_events(&mut self) -> Result<Vec<Event>> {
+    async fn get_events(&self) -> Result<Vec<Event>> {
         Ok(self
             .g_client
             .list(
@@ -65,4 +67,17 @@ impl DataHandler {
             )
             .await?)
     }
+}
+
+async fn create_db(sql_conn: &Connection) -> Result<()> {
+    sql_conn.execute(
+        "CREATE TABLE IF NOT EXISTS done (
+            event_id INTEGER NOT NULL,
+            calendar_id INTEGER NOT NULL,
+            timestamp INTEGER NOT NULL,
+            PRIMARY KEY (event_id, calendar_id)
+        )",
+        (),
+    )?;
+    Ok(())
 }
